5dc29cc55e04d9697b3912f726e7a59e7870b7f0
ohsnaplol <emyb@bellsouth.net>
2018-01-26 15:33:21 -0500

    add to inventory (bugged)

--- shamazonManager.js
+++ shamazonManager.js
@@ -17,7 +17,7 @@ function showMenu() {
       type: 'list',
       name: 'choice',
       message: `Manager View`,
-      choices: ['View Products on Sale', 'View Low Inventory', 'Add to Intentory', 'Add to Product', 'Quit']
+      choices: ['View Products on Sale', 'View Low Inventory', 'Add to Inventory', 'Add to Product', 'Quit']
     }
   ]).then(function(answers) {
     if (answers.choice === 'View Products on Sale') {
@@ -27,7 +27,8 @@ function showMenu() {
       viewLowInventory()
     }
     if (answers.choice === 'Add to Inventory') {
-
+      console.log('add to inventory chosen')
+      addToInventory()
     }
     if (answers.choice === 'Add to Product') {
 
@@ -64,5 +65,29 @@ function viewLowInventory() {
 }
 
 function addToInventory() {
-
+  // get list of products
+  connection.query(`SELECT * FROM products`, function(error, result) {
+    var choicesArr = []
+    if (error) throw error
+    for (var i = 0; i < result.length; i++) {
+      choicesArr.push(result[i].product_name)
+    }
+    inquirer.prompt([
+      {
+        name: 'new_product',
+        type: 'list',
+        message: 'Which item would you like to add inventory to?',
+        choices: choicesArr
+      },
+      {
+        name: 'quantity',
+        type: 'input',
+        message: 'How many would you like to add?'
+      }
+    ]).then(function(answers) {
+      console.log(`You chose ${answers.new_product} with quantity ${answers.quantity}`)
+      connection.query(`UPDATE products SET stock_quantity = stock_quantity + ${answers.quantity} WHERE product_name = ${answers.new_product}`) // updates product by name instead of by id. This should be changed
+      showMenu()
+    })
+  })
 }
